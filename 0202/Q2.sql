d663c459f400512681812880cbf4370484b2eafc
성현준 <60638615+sunghj1118@users.noreply.github.com>
2023-02-06 22:58:47 +0900

    Second attempt at Q2
    Facing runtime error with no results

--- 0202/Q2.sql
+++ 0202/Q2.sql
@@ -1,5 +1,5 @@
-SELECT a.ProductID, SUM(Quantity) as TotalQuantity, SUM(Quantity) * Price as TotalPrice
-FROM OrderDetails as a
-JOIN Products as b
-ON a.ProductID = b.ProductID
-GROUP BY a.ProductID
+SELECT CustomerName
+FROM Customers as a
+JOIN OrderDetails as b, Orders as c, Products as d
+WHERE (b.Quantity * d.Price) >= 10000
+GROUP BY CustomerName

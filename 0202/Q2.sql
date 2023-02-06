5e040dae99280e8cf991240fd5128a40cdb1823e
성현준 <60638615+sunghj1118@users.noreply.github.com>
2023-02-06 23:22:07 +0900

    Success

--- 0202/Q2.sql
+++ 0202/Q2.sql
@@ -1,5 +1,7 @@
 SELECT CustomerName
 FROM Customers as a
-JOIN OrderDetails as b, Orders as c, Products as d
+JOIN Orders as c on a.CustomerID = c.CustomerID
+JOIN OrderDetails as b on c.OrderID = b.OrderID
+JOIN Products as d on b.ProductID = d.ProductID
 WHERE (b.Quantity * d.Price) >= 10000
 GROUP BY CustomerName;
